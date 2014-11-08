29a9b8364f3f493f807f243fa41bca87ccc0cdf4
Migariane <watzilei@hotmail.com>
2014-11-08 14:48:58 -0500

    script

---
+++ assign_drug.py
@@ -0,0 +1,12 @@
+def assign_drug(filename):
+    number=filename[13:-4]
+    result=''
+    if(int(number)%2)==1
+    result='tylenol'
+    else:
+    result='placebo'
+    return result
+import sys
+filename = sys.arg[1]
+print filename
+

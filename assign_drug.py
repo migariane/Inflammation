5f23d0893384c93ff0ecdc379fa65b21142ccd3f
Migariane <watzilei@hotmail.com>
2014-11-08 14:56:03 -0500

    gitignore

--- assign_drug.py
+++ assign_drug.py
@@ -1,12 +1,17 @@
 def assign_drug(filename):
-    number=filename[13:-4]
-    result=''
-    if(int(number)%2)==1
-    result='tylenol'
+    number = filename[13:-4]
+    result = ''
+    if(int(number)%2) == 1:
+      result = 'tylenol'
     else:
-    result='placebo'
+      result = 'placebo'
     return result
+
 import sys
+
 filename = sys.arg[1]
 print filename
 
+assert assign_drug("inflammation_1.dat") == "tylenol"
+assert assign_drug("inflammation_2.dat") == "placebo"
+assert assign_drug("inflammation_3.dat") == "tylenol"
